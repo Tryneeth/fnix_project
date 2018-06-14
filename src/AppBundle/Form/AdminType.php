e5ca4be7733a4329d8bb39fc952081a80c71f8d0
Escobar <escobar@aleph.engineering>
2018-06-14 14:57:42 -0400

    Base template files with new design Admin Views with new Design

--- src/AppBundle/Form/AdminType.php
+++ src/AppBundle/Form/AdminType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -21,16 +22,47 @@ class AdminType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username',TextType::class)
+            ->add('username',TextType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            )
             ->add('password',RepeatedType::class,[
-                'type'=> PasswordType::class
+                'type'=> PasswordType::class,
+                'required' => true,
+                'first_options'  => array('label' => 'Password', 'attr' => ['class' => 'form-control']),
+                'second_options' => array('label' => 'Repeat Password', 'attr' => ['class' => 'form-control']),
             ])
-            ->add('role', TextType::class)
-            ->add('name',TextType::class)
-            ->add('lastname',TextType::class)
-            ->add('email',EmailType::class)
-            ->add('phone',NumberType::class)
-            ->add('photo');
+            ->add('role', TextType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            )
+            ->add('name',TextType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            )
+            ->add('lastname',TextType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            )
+            ->add('email',EmailType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            )
+            ->add('phone',NumberType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            )
+            ->add('photo', FileType::class,
+	            ['attr' =>
+		             ['class' => 'form-control']
+	            ]
+            );
     }/**
      * {@inheritdoc}
      */
